27120a3f2448847dad2dcfdfe925eb436591482a
Thomas Passmore <thomas@ecosuite.io>
2024-09-05 00:50:19 +1200

    Add import sample
    Signed-off-by: Thomas Passmore <thomas@ecosuite.io>

--- solarnetwork_python/client.py
+++ solarnetwork_python/client.py
@@ -1,13 +1,81 @@
 from datetime import datetime
+import json
 import requests
 
 from solarnetwork_python.authentication import generate_auth_header, get_x_sn_date
-
 
 class Client:
     def __init__(self, token: str, secret: str) -> None:
         self.token = token
         self.secret = secret
+
+    # Description is a dictionary that looks like this:
+    # {
+    #   "name": "Test Import",
+    #   "stage": true,
+    #   "inputConfiguration": {
+    #     "name": "Test Input",
+    #     "timeZoneId": "America/New_York",
+    #     "serviceIdentifier": "net.solarnetwork.central.datum.imp.standard.BasicCsvDatumImportInputFormatService",
+    #     "serviceProperties": {
+    #       "headerRowCount": "0",
+    #       "dateColumnsValue": "1",
+    #       "dateFormat": "MM/dd/yyyy HH:mm:ss",
+    #       "nodeIdColumn": "2",
+    #       "sourceIdColumn": "3"
+    #     }
+    #   }
+    # }
+    # 
+    # Data is the actual CSV data in string form.
+    def import_data(self, description, data):
+        # The time has to be in UTC
+        now = datetime.utcnow()
+        date = get_x_sn_date(now)
+        path = "/solaruser/api/v1/sec/user/import/jobs"
+
+        # These should be present for all API calls
+        headers = {"content-type": "multipart/form-data; charset=utf-8; boundary=__X_PAW_BOUNDARY__", "host": "data.solarnetwork.net", "x-sn-date": date}
+
+        description_data = json.dumps(description)
+
+        body = (
+            f'--__X_PAW_BOUNDARY__\r\n'
+            f'Content-Disposition: form-data; name="config"\r\n'
+            f'Content-Type: application/json\r\n'
+            f'\r\n'
+            f'{description_data}\r\n'
+            f'--__X_PAW_BOUNDARY__\r\n'
+            f'Content-Disposition: form-data; name="data"; filename="data.csv"\r\n'
+            f'Content-Type: text/csv\r\n'
+            f'\r\n'
+            f'{data}\r\n'
+            f'--__X_PAW_BOUNDARY__--\r\n'
+        )
+
+        auth = generate_auth_header(
+            self.token, self.secret, "POST", path, "", headers, body, now
+        )
+
+        resp = requests.post(
+            url="https://data.solarnetwork.net/solaruser/api/v1/sec/user/import/jobs",
+            data=body,
+
+            # Make sure to actually include the headers given by the previous
+            # headers argument
+            headers={
+                "Content-Type": "multipart/form-data; charset=utf-8; boundary=__X_PAW_BOUNDARY__",
+                "host": "data.solarnetwork.net",
+                "x-sn-date": date,
+                "Authorization": auth,
+            },
+        )
+
+        v = resp.json()
+        if v["success"] != True:
+            raise Exception("Unsuccessful API call")
+
+        return v["data"]
 
     def nodes(self) -> list[int]:
         # Documentation for this route is here:
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,7 +1,41 @@
 from solarnetwork_python.client import Client
 
-
 def test_nodes():
     client = Client("token", "secret")
     nodes = client.nodes()
     assert len(nodes) > 1
+
+def test_import():
+    client = Client("token", "secret")
+
+    service_properties = {
+        "headerRowCount": "1",
+        "dateColumnsValue": "3",
+        "dateFormat": "MM/dd/yyyy HH:mm:ss",
+        "nodeIdColumn": "1",
+        "sourceIdColumn": "2"
+    }
+
+    inner = {
+        "name": "Test Input",
+        "timeZoneId": "America/New_York",
+        "serviceIdentifier": "net.solarnetwork.central.datum.imp.standard.BasicCsvDatumImportInputFormatService",
+        "serviceProperties": service_properties
+    }
+
+    outer = {
+        "name": "Test Import",
+        "stage": True,
+        "inputConfiguration": inner
+    }
+
+    csv_data = (
+        'node,source,date,irradiance,wattHours\r\n'
+        '708,/TEST/TEST,2024-01-21 07:25:00,0.000,0.000\r\n'
+        '708,/TEST/TEST,2024-01-21 07:30:00,9.000,10.100\r\n'
+        '708,/TEST/TEST,2024-01-21 07:35:00,12.336,15.020\r\n'
+        '708,/TEST/TEST,2024-01-21 07:40:00,15.660,19.325\r\n'
+        '708,/TEST/TEST,2024-01-21 07:45:00,18.996,25.923'
+    )
+
+    client.import_data(outer, csv_data)
